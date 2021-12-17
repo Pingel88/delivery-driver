9f61c1fcac6ad02b0695bd07c265319e86bc8a08
Michael Pingel <mdpingel@gmail.com>
2021-12-16 23:08:55 -0800

    end of 19. Using OnTrigger2D()

--- Delivery Driver/Assets/Collision.cs
+++ Delivery Driver/Assets/Collision.cs
@@ -8,4 +8,9 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("Boink!");
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Finish!");
+    }
 }

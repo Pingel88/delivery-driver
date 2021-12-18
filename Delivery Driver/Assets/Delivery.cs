06d49c5fd53b79d19b86ba1f80aef0ffefd53c80
Michael Pingel <mdpingel@gmail.com>
2021-12-17 20:19:15 -0800

    end of 23. If Statements & Tags

--- Delivery Driver/Assets/Delivery.cs
+++ Delivery Driver/Assets/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -11,6 +11,13 @@ public class Collision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Finish!");
+        if (other.tag == "Package")
+        {
+            Debug.Log("Package picked up!");
+        }
+        if (other.tag == "Customer")
+        {
+            Debug.Log("Package delivered!");
+        }
     }
 }

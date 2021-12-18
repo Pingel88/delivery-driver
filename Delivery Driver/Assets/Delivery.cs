2a8ad0e31d3b5b7c387c5011912110571e0509e0
Michael Pingel <mdpingel@gmail.com>
2021-12-17 20:31:17 -0800

    end of 24. How To Use Bools

--- Delivery Driver/Assets/Delivery.cs
+++ Delivery Driver/Assets/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Boink!");
@@ -11,13 +13,15 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up!");
+            hasPackage = true;
         }
-        if (other.tag == "Customer")
+        if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered!");
+            hasPackage = false;
         }
     }
 }
